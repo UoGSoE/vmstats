eabed3d5e682eaf678f3ab057dc83d702ca93bb9
Shift <shift@laravelshift.com>
2025-02-14 11:57:40 +0000

    Adopt Laravel type hints

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,7 +12,7 @@ class Server extends Model
 
     protected $fillable = ['name', 'notes'];
 
-    public function guests()
+    public function guests(): HasMany
     {
         return $this->hasMany(Guest::class);
     }
