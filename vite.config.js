c129b3aef56b74e6a3beed2f421aa288fcd8969d
Finlay Maciver <finlay.maciver@glasgow.ac.uk>
2025-02-14 12:24:43 +0000

    Converted to Vite

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        https: false,
+        host: true,
+        port: 3009,
+        hmr: {host: 'localhost', protocol: 'ws'},
+    },
 });
